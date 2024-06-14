bd60d07123898820681a46bd04a538f96ee11de2
Rishi Patodiya <114613978+TaskmasterRishi@users.noreply.github.com>
2024-06-14 22:58:33 +0530

    navbar complete

--- js/rishi.js
+++ js/rishi.js
@@ -1,5 +1,5 @@
 // Typing Effect
-var TxtType = function(el, toRotate, period) {
+var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
@@ -9,17 +9,17 @@ var TxtType = function(el, toRotate, period) {
   this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function() {
+TxtType.prototype.tick = function () {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
 
   if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
   var that = this;
   var delta = 200 - Math.random() * 100;
@@ -27,27 +27,27 @@ TxtType.prototype.tick = function() {
   if (this.isDeleting) { delta /= 2; }
 
   if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
+    delta = this.period;
+    this.isDeleting = true;
   } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
   }
 
-  setTimeout(function() {
-      that.tick();
+  setTimeout(function () {
+    that.tick();
   }, delta);
 };
 
-window.onload = function() {
+window.onload = function () {
   var elements = document.getElementsByClassName('typewrite');
-  for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-type');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
   }
   // INJECT CSS
   var css = document.createElement("style");
@@ -63,44 +63,49 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-hamburger.classList.toggle("active");
-navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
 document.addEventListener("click", function (event) {
-const isClickInsideNav = navMenu.contains(event.target);
-const isClickInsideHamburger = hamburger.contains(event.target);
+  const isClickInsideNav = navMenu.contains(event.target);
+  const isClickInsideHamburger = hamburger.contains(event.target);
 
-if (!isClickInsideNav && !isClickInsideHamburger) {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
+  if (!isClickInsideNav && !isClickInsideHamburger) {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
 
-if (event.target.classList.contains("nav-link")) {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
+  if (event.target.classList.contains("nav-link")) {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
 });
 
 // Navbar Scroll Effect
-window.addEventListener('scroll', function() {
-const scrollThreshold = window.innerHeight * 0.6; 
-const navbar = document.querySelector('.navbar');
-
-if (window.scrollY > scrollThreshold) {
+window.addEventListener('scroll', function () {
+  const scrollThreshold = window.innerHeight * 0.6;
+  const navbar = document.querySelector('.navbar');
+  const name = this.document.querySelector('.name')
+  if (window.scrollY > scrollThreshold) {
     navbar.classList.add('navbar-active');
-} else {
+    name.classList.add('show-name');
+    name.classList.remove('hide-name');
+
+  } else {
     navbar.classList.remove('navbar-active');
-}
+    name.classList.remove('show-name');
+    name.classList.add('hide-name');
+  }
 });
 
-window.addEventListener('scroll', function() {
-  const scrollThreshold = window.innerHeight * 0.9; 
+window.addEventListener('scroll', function () {
+  const scrollThreshold = window.innerHeight * 0.8;
   const navbar = document.querySelector('.navbar');
   const banLogo = navbar.querySelector('navlogo')
   if (window.scrollY > scrollThreshold) {
-      navbar.style.position = "fixed"
+    navbar.classList.add('sticky')
   } else {
-      navbar.style.position = "relative"
+    navbar.classList.remove('sticky')
   }
-  });
+});
