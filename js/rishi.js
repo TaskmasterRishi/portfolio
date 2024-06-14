f8419ba551a56b9a4cbed6a9173983f3997aebf4
Rishi Patodiya <114613978+TaskmasterRishi@users.noreply.github.com>
2024-06-14 09:48:21 +0530

    navbar dessign

--- js/rishi.js
+++ js/rishi.js
@@ -1,81 +1,95 @@
+// Typing Effect
 var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
 };
 
 TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+  if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-    var that = this;
-    var delta = 200 - Math.random() * 100;
+  var that = this;
+  var delta = 200 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) { delta /= 2; }
 
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-    }
+  if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+  }
 
-    setTimeout(function() {
-    that.tick();
-    }, delta);
+  setTimeout(function() {
+      that.tick();
+  }, delta);
 };
 
 window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
           new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
-    document.body.appendChild(css);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
+  document.body.appendChild(css);
 };
 
+// Hamburger Menu
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+hamburger.classList.toggle("active");
+navMenu.classList.toggle("active");
 }
 
 document.addEventListener("click", function (event) {
-  const isClickInsideNav = navMenu.contains(event.target);
-  const isClickInsideHamburger = hamburger.contains(event.target);
+const isClickInsideNav = navMenu.contains(event.target);
+const isClickInsideHamburger = hamburger.contains(event.target);
 
-  if (!isClickInsideNav && !isClickInsideHamburger) {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  }
+if (!isClickInsideNav && !isClickInsideHamburger) {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
 
-  if (event.target.classList.contains("nav-link")) {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  }
+if (event.target.classList.contains("nav-link")) {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+});
+
+// Navbar Scroll Effect
+window.addEventListener('scroll', function() {
+const scrollThreshold = window.innerHeight * 0.2; 
+const navbar = document.querySelector('.navbar');
+
+if (window.scrollY > scrollThreshold) {
+    navbar.classList.add('navbar-active');
+} else {
+    navbar.classList.remove('navbar-active');
+}
 });
